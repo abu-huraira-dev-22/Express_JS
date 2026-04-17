9c9b213ff34babe7118cb26b3ba152076ca854ad
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-04-17 18:33:06 +0500

    put and delete method

--- requests.js
+++ requests.js
@@ -2,7 +2,7 @@
 import express from "express";
 const app = express();
 app.use(express.json());
-const users = [];
+let users = [];
 app.get("/users", (req, res) => {
   res.json({
     status: true,
@@ -20,6 +20,33 @@ app.get('/users/:username',(req,res)=>{
     data: users.filter((user)=>user.username==username)
   })
 })
+
+app.put('/users/:username',(req,res)=>{
+  const {username} = req.params
+  users = users.map((user)=>{
+    if(user.username == username){
+      user = req.body
+      return user
+    }
+    return user
+  })
+  res.json({
+     status:true,
+    message: 'Update User Data Successfully',
+  })
+})
+
+app.delete('/users/:username',(req,res)=>{
+  const {username} = req.params
+  users = users.filter((user)=>{
+    return user.username != username
+  })
+    res.json({
+    status: true,
+    message: "Users Deleted Successfully",
+  });
+})
+
 app.post("/users", (req, res) => {
   const data = req.body;
   users.push(data);
